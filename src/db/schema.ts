7b1ceb0799322e300aa4e740dcdc2ea46c5160ad
veecodekub <natsukashiiz.dev@gmail.com>
2025-12-14 03:22:30 +0700

    feat: add unique index for short_code in shortUrlsTable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,27 @@
-import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core';
+import {
+    integer,
+    pgTable,
+    varchar,
+    timestamp,
+    uniqueIndex,
+} from 'drizzle-orm/pg-core';
 
-export const shortUrlsTable = pgTable('short_urls', {
-    id: integer('id').primaryKey().generatedAlwaysAsIdentity({
-        startWith: 1001,
-    }),
-    long_url: varchar('long_url').notNull(),
-    short_code: varchar('short_code').notNull().unique(),
-    clicks: integer('clicks').notNull().default(0),
-    created_at: timestamp('created_at', { withTimezone: true })
-        .notNull()
-        .defaultNow(),
-    updated_at: timestamp('updated_at', { withTimezone: true })
-        .notNull()
-        .defaultNow()
-        .$onUpdate(() => new Date()),
-});
+export const shortUrlsTable = pgTable(
+    'short_urls',
+    {
+        id: integer('id').primaryKey().generatedAlwaysAsIdentity({
+            startWith: 1001,
+        }),
+        long_url: varchar('long_url').notNull(),
+        short_code: varchar('short_code').notNull(),
+        clicks: integer('clicks').notNull().default(0),
+        created_at: timestamp('created_at', { withTimezone: true })
+            .notNull()
+            .defaultNow(),
+        updated_at: timestamp('updated_at', { withTimezone: true })
+            .notNull()
+            .defaultNow()
+            .$onUpdate(() => new Date()),
+    },
+    (table) => [uniqueIndex('short_code_idx').on(table.short_code)]
+);
