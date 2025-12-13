731014472d6de66f35087b7a1f2d13673e75dc7a
veecodekub <natsukashiiz.dev@gmail.com>
2025-12-13 17:02:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# url-shortener
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run development server:
+
+```bash
+bun dev
+```
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World with Bun and Express!');
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
