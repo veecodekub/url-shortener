31fb5f7fc29562c59bc054dd73ab6118c72601b7
veecodekub <natsukashiiz.dev@gmail.com>
2025-12-13 19:06:50 +0700

    feat: add get health api endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.send('Hello World with Bun and Express!');
 });
 
+app.get('/health', (_, res) => {
+    res.json({
+        status: 'ok',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+    });
+});
+
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
